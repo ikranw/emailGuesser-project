760cf8bc74ce0ca51c9168b5f09ab6a422882995
Ikran Warsame <141252436+ikranw@users.noreply.github.com>
2024-04-23 21:11:15 -0400

    Update unit_test.py

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -2,16 +2,12 @@ import pytest
 from emailGuesser.py import get_user_input
 
 # Test 1: test to make sure name and surname inputs/arguments are strings not numbers. 
-#STRING
-def test_name_input():
-    with pytest.raises(nameType) as error:
-        convert_to_letter_grade(1)
-    assert str(error.value) == "Name and surname inputs must be written in letters."
-#LIST
-    with pytest.raises(nameType) as error:
-        convert_to_letter_grade([0, 1, 2])
-    assert str(error.value) == "Name and surname inputs must be written in letters."
-#NONE
-    with pytest.raises(nameType) as error:
-        convert_to_letter_grade(None)
-    assert str(error.value) == "Name and surname inputs must be written in letters."
+import pytest
+from emailGuesser import get_user_input
+
+# Test 1: test to make sure name and surname inputs/arguments are strings not numbers.
+def test_name_input_string():
+    with pytest.raises(ValueError) as error:
+        get_user_input()
+    assert str(error.value) == "Name and surname inputs must contain only letters."
+
