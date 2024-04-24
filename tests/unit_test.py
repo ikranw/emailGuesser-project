5e7d87ff6b0672c963df4cf0bbb14196fa8f966c
Ikran Warsame <141252436+ikranw@users.noreply.github.com>
2024-04-23 21:11:45 -0400

    Update unit_test.py

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,10 +1,6 @@
 import pytest
 from emailGuesser.py import get_user_input
 
-# Test 1: test to make sure name and surname inputs/arguments are strings not numbers. 
-import pytest
-from emailGuesser import get_user_input
-
 # Test 1: test to make sure name and surname inputs/arguments are strings not numbers.
 def test_name_input_string():
     with pytest.raises(ValueError) as error:
