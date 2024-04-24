dcd831b409e2c3d82939f0c21a7a61a7cd9ad776
Ikran Warsame <141252436+ikranw@users.noreply.github.com>
2024-04-23 22:22:27 -0400

    Update unit_test.py

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,9 +1,4 @@
 import pytest
-from emailGuesser.py import get_user_input
+from Functions import get_user_input, 
 
-# Test 1: test to make sure name and surname inputs/arguments are strings not numbers.
-def test_name_input_string():
-    with pytest.raises(ValueError) as error:
-        get_user_input()
-    assert str(error.value) == "Name and surname inputs must contain only letters."
 
